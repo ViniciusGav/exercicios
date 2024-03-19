0005a2c08984d49308247e4df2c407d4bf181e0d
ViniciusGav <vinicius_gava@estudante.sesisenai.org.br>
2024-03-19 19:38:35 -0300

    atualizando git

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "PiguiMeu";
+        funcionario.departamento= "Compras";
+        funcionario.salario = 3000;
+        funcionario.aumentarSalario(10);
+    }
+}
